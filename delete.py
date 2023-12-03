8bef384ca2a10eccd5c399848288a978217dc096
Emir <104788503+Lantanchick@users.noreply.github.com>
2023-12-03 16:12:30 +0300

    Update delete.py

--- delete.py
+++ delete.py
@@ -15,8 +15,7 @@ KB1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 kb1_1 = KeyboardButton("/help")
 kb1_2 = KeyboardButton("/🌠")
 kb1_3 = KeyboardButton("/🌤")
-kb1_4 = KeyboardButton("/✨")
-KB1.add(kb1_1).insert(kb1_4)
+KB1.add(kb1_1)
 KB1.add(kb1_3).insert(kb1_2)
 
 KB3 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
@@ -33,10 +32,6 @@ def gen_photo_url(word: str) -> str:
 
 def gen_weather_outer(city):
     return "https://yandex.ru/weather/ru-RU/" + city + "/details"
-
-
-def gen_video_url(word):
-    return "https://www.youtube.com/results?search_query=" + word
 
 
 @dp.message_handler(commands=['start'])
@@ -69,15 +64,6 @@ async def echo(message: types.Message):
         else:
             await bot.send_message(chat_id=message.from_user.id,
                                    text=gen_weather_outer(user_text))
-            await message.delete()
-            user_text = None
-    elif message.text == '/✨':
-        if user_text is None:
-            await bot.send_message(chat_id=message.from_user.id,
-                                   text="вы еще ничего не ввели")
-        else:
-            await bot.send_message(chat_id=message.from_user.id,
-                                   text=gen_video_url(user_text))
             await message.delete()
             user_text = None
 
